70bf1009c813795e14610c0a1552254a4c0a5470
Axel Venet <avenet@wamiz.com>
2022-02-20 01:32:00 +0100

    Migrate commander actions

---
+++ src/Modules/Ares/Infrastructure/Controller/Commander/Disband.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Modules\Ares\Infrastructure\Controller\Commander;
+
+use App\Classes\Entity\EntityManager;
+use App\Classes\Exception\ErrorException;
+use App\Modules\Ares\Manager\CommanderManager;
+use App\Modules\Zeus\Model\Player;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class Disband extends AbstractController
+{
+	public function __invoke(
+		Request $request,
+		Player $currentPlayer,
+		CommanderManager $commanderManager,
+		EntityManager $entityManager,
+		int $id,
+	): Response {
+		if (($commander = $commanderManager->get($id)) === null || $commander->rPlayer !== $currentPlayer->getId()) {
+			throw new ErrorException('Ce commandant n\'existe pas ou ne vous appartient pas.');
+		}
+		if ($commander->statement !== 1) {
+			throw new ErrorException('Vous ne pouvez pas retirer les vaisseaux à un officier en déplacement.');
+		}
+
+		// vider le commandant
+		$commanderManager->emptySquadrons($commander);
+
+		$this->addFlash('success', 'Vous avez vidé l\'armée menée par votre commandant ' . $commander->getName() . '.');
+
+		$entityManager->flush();
+
+		return $this->redirect($request->headers->get('referer'));
+	}
+}
---
+++ src/Modules/Ares/Infrastructure/Controller/Commander/Fire.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Modules\Ares\Infrastructure\Controller\Commander;
+
+use App\Classes\Entity\EntityManager;
+use App\Classes\Exception\ErrorException;
+use App\Modules\Ares\Manager\CommanderManager;
+use App\Modules\Zeus\Model\Player;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class Fire extends AbstractController
+{
+	public function __invoke(
+		Request $request,
+		Player $currentPlayer,
+		CommanderManager $commanderManager,
+		EntityManager $entityManager,
+		int $id,
+	): Response {
+		if (($commander = $commanderManager->get($id)) === null || $commander->rPlayer !== $currentPlayer->getId()) {
+			throw new ErrorException('Ce commandant n\'existe pas ou ne vous appartient pas.');
+		}
+
+		if ($commander->statement == 1) {
+			// vider le commandant
+			$commanderManager->emptySquadrons($commander);
+			$commander->setStatement(4);
+
+			$this->addFlash('success', 'Vous avez renvoyé votre commandant ' . $commander->getName() . '.');
+		} else {
+			$this->addFlash('error', 'Vous ne pouvez pas renvoyer un officier en déplacement.');
+		}
+
+		$entityManager->flush();
+
+		return $this->redirectToRoute('fleet_headquarters');
+	}
+}
