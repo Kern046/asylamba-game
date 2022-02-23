edf0461ebc7e24cfc76275738896003a9dd4ceca
Axel Venet <avenet@wamiz.com>
2022-02-23 18:42:39 +0100

    Migrate university view

--- src/Modules/Zeus/Infrastructure/Twig/PlayerExtension.php
+++ src/Modules/Zeus/Infrastructure/Twig/PlayerExtension.php
@@ -4,12 +4,13 @@ namespace App\Modules\Zeus\Infrastructure\Twig;
 
 use App\Modules\Athena\Manager\OrbitalBaseManager;
 use App\Modules\Demeter\Resource\ColorResource;
+use App\Modules\Promethee\Helper\ResearchHelper;
 use Twig\Extension\AbstractExtension;
 use Twig\TwigFunction;
 
 class PlayerExtension extends AbstractExtension
 {
-	public function __construct(protected OrbitalBaseManager $orbitalBaseManager)
+	public function __construct(protected OrbitalBaseManager $orbitalBaseManager, protected ResearchHelper $researchHelper)
 	{
 
 	}
@@ -18,7 +19,8 @@ class PlayerExtension extends AbstractExtension
 	{
 		return [
 			new TwigFunction('get_faction_info', fn (int $factionId, string $info) => ColorResource::getInfo($factionId, $info)),
-			new TwigFunction('get_player_bases_count', fn (array $movingCommanders) => $this->orbitalBaseManager->getPlayerBasesCount($movingCommanders))
+			new TwigFunction('get_player_bases_count', fn (array $movingCommanders) => $this->orbitalBaseManager->getPlayerBasesCount($movingCommanders)),
+			new TwigFunction('get_research_info', fn (string $researchType, string $info) => $this->researchHelper->getInfo($researchType, $info)),
 		];
 	}
 }
