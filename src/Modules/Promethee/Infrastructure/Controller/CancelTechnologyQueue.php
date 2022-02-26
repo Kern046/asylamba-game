3615bb8f969635c2df5715059f77cc2c75d131d9
Axel Venet <avenet@wamiz.com>
2022-02-26 21:27:04 +0100

    Migrate technology queue actions

---
+++ src/Modules/Promethee/Infrastructure/Controller/CancelTechnologyQueue.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace App\Modules\Promethee\Infrastructure\Controller;
+
+use App\Classes\Entity\EntityManager;
+use App\Classes\Exception\ErrorException;
+use App\Classes\Library\Utils;
+use App\Modules\Athena\Manager\OrbitalBaseManager;
+use App\Modules\Athena\Model\OrbitalBase;
+use App\Modules\Promethee\Helper\TechnologyHelper;
+use App\Modules\Promethee\Manager\TechnologyQueueManager;
+use App\Modules\Promethee\Model\TechnologyQueue;
+use App\Modules\Zeus\Manager\PlayerManager;
+use App\Modules\Zeus\Model\Player;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class CancelTechnologyQueue extends AbstractController
+{
+	public function __invoke(
+		Request $request,
+		Player $currentPlayer,
+		OrbitalBase $currentBase,
+		TechnologyHelper $technologyHelper,
+		TechnologyQueueManager $technologyQueueManager,
+		PlayerManager $playerManager,
+		OrbitalBaseManager $orbitalBaseManager,
+		EntityManager $entityManager,
+		string $identifier,
+	): Response {
+		if ($technologyHelper->isATechnology($identifier)) {
+			$placeTechnologyQueues = $technologyQueueManager->getPlaceQueues($currentBase->getId());
+
+			$index = NULL;
+			$targetLevel = 0;
+			$nbQueues = count($placeTechnologyQueues);
+			for ($i = 0; $i < $nbQueues; $i++) {
+				$queue = $placeTechnologyQueues[$i];
+				// get the queue to delete
+				if ($queue->technology == $identifier AND $queue->targetLevel > $targetLevel) {
+					$index = $i;
+					$targetLevel = $queue->targetLevel;
+					$dStart = $queue->dStart;
+				}
+			}
+
+			# if it's the first, the next must restart by now
+			if ($index == 0) {
+				$dStart = Utils::now();
+			}
+
+			if ($index !== NULL) {
+				# shift
+				for ($i = $index + 1; $i < $nbQueues; $i++) {
+					$queue = $placeTechnologyQueues[$i];
+
+					// $oldDate = $queue->dEnd;
+					$queue->dEnd = Utils::addSecondsToDate($dStart, Utils::interval($queue->dStart, $queue->dEnd, 's'));
+					$queue->dStart = $dStart;
+					// @TODO handle rescheduling
+					// $scheduler->reschedule($queue, $queue->dEnd, $oldDate);
+
+					$dStart = $queue->dEnd;
+				}
+
+				// @TODO handle cancellation
+				//$scheduler->cancel($placeTechnologyQueues[$index], $placeTechnologyQueues[$index]->getEndedAt());
+				$entityManager->remove($placeTechnologyQueues[$index]);
+				$entityManager->flush(TechnologyQueue::class);
+
+				$technologyResourceRefund = $this->getParameter('promethee.technology_queue.resource_refund');
+				$technologyCreditRefund = $this->getParameter('promethee.technology_queue.credit_refund');
+				// rends les ressources et les crédits au joueur
+				$resourcePrice = $technologyHelper->getInfo($identifier, 'resource', $targetLevel);
+				$resourcePrice *= $technologyResourceRefund;
+				$orbitalBaseManager->increaseResources($currentBase, $resourcePrice, true);
+				$creditPrice = $technologyHelper->getInfo($identifier, 'credit', $targetLevel);
+				$creditPrice *= $technologyCreditRefund;
+				$playerManager->increaseCredit($currentPlayer, $creditPrice);
+				$this->addFlash('success', 'Construction annulée, vous récupérez le ' . $technologyResourceRefund * 100 . '% des ressources ainsi que le ' . $technologyCreditRefund * 100 . '% des crédits investis pour le développement');
+
+				return $this->redirect($request->headers->get('referer'));
+			} else {
+				throw new ErrorException('impossible d\'annuler la technologie');
+			}
+		} else {
+			throw new ErrorException('la technologie indiquée n\'est pas valide');
+		}
+	}
+}
---
+++ src/Modules/Promethee/Infrastructure/Controller/SearchTechnology.php
@@ -0,0 +1,136 @@
+<?php
+
+namespace App\Modules\Promethee\Infrastructure\Controller;
+
+use App\Classes\Exception\ErrorException;
+use App\Classes\Library\Game;
+use App\Classes\Library\Utils;
+use App\Modules\Athena\Manager\OrbitalBaseManager;
+use App\Modules\Athena\Model\OrbitalBase;
+use App\Modules\Demeter\Resource\ColorResource;
+use App\Modules\Promethee\Helper\TechnologyHelper;
+use App\Modules\Promethee\Manager\ResearchManager;
+use App\Modules\Promethee\Manager\TechnologyManager;
+use App\Modules\Promethee\Manager\TechnologyQueueManager;
+use App\Modules\Promethee\Model\Technology;
+use App\Modules\Promethee\Model\TechnologyQueue;
+use App\Modules\Zeus\Helper\TutorialHelper;
+use App\Modules\Zeus\Manager\PlayerManager;
+use App\Modules\Zeus\Model\Player;
+use App\Modules\Zeus\Model\PlayerBonus;
+use App\Modules\Zeus\Resource\TutorialResource;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class SearchTechnology extends AbstractController
+{
+	public function __invoke(
+		Request $request,
+		Player $currentPlayer,
+		OrbitalBase $currentBase,
+		TechnologyHelper $technologyHelper,
+		TechnologyManager $technologyManager,
+		TechnologyQueueManager $technologyQueueManager,
+		OrbitalBaseManager $orbitalBaseManager,
+		PlayerManager $playerManager,
+		TutorialHelper $tutorialHelper,
+		ResearchManager $researchManager,
+		string $identifier,
+	): Response {
+		$session = $request->getSession();
+		if ($technologyHelper->isATechnology($identifier) && !$technologyHelper->isATechnologyNotDisplayed($identifier)) {
+			if (($technologyQueueManager->getPlayerTechnologyQueue($currentPlayer->getId(), $identifier)) === null) {
+
+				$technos = $technologyManager->getPlayerTechnology($currentPlayer->getId());
+				$targetLevel = $technos->getTechnology($identifier) + 1;
+				// @TODO I think this piece of code is dead
+				$technologyQueues = $technologyQueueManager->getPlaceQueues($currentBase->getId());
+				$nbTechnologyQueues = count($technologyQueues);
+				foreach ($technologyQueues as $technologyQueue) {
+					if ($technologyQueue->technology == $identifier) {
+						$targetLevel++;
+					}
+				}
+				$researchManager->load(array('rPlayer' => $currentPlayer->getId()));
+
+				if ($technologyHelper->haveRights($identifier, 'resource', $targetLevel, $currentBase->getResourcesStorage())
+					AND $technologyHelper->haveRights($identifier, 'credit', $targetLevel, $currentPlayer->getCredit())
+					AND $technologyHelper->haveRights($identifier, 'queue', $currentBase, $nbTechnologyQueues)
+					AND $technologyHelper->haveRights($identifier, 'levelPermit', $targetLevel)
+					AND $technologyHelper->haveRights($identifier, 'technosphereLevel', $currentBase->getLevelTechnosphere())
+					AND ($technologyHelper->haveRights($identifier, 'research', $targetLevel, $researchManager->getResearchList($researchManager->get())) === TRUE)
+					AND $technologyHelper->haveRights($identifier, 'maxLevel', $targetLevel)
+					AND $technologyHelper->haveRights($identifier, 'baseType', $currentBase->typeOfBase)) {
+
+					# tutorial
+					if ($currentPlayer->stepDone == FALSE) {
+						switch ($currentPlayer->getStepTutorial()) {
+							case TutorialResource::SHIP0_UNBLOCK:
+								if ($identifier == Technology::SHIP0_UNBLOCK) {
+									$tutorialHelper->setStepDone();
+								}
+								break;
+							case TutorialResource::SHIP1_UNBLOCK:
+								if ($identifier == Technology::SHIP1_UNBLOCK) {
+									$tutorialHelper->setStepDone();
+								}
+								break;
+						}
+					}
+
+					// construit la nouvelle techno
+					$time = $technologyHelper->getInfo($identifier, 'time', $targetLevel);
+					$bonusPercent = $session->get('playerBonus')->get(PlayerBonus::TECHNOSPHERE_SPEED);
+					if ($currentPlayer->getRColor() == ColorResource::APHERA) {
+						# bonus if the player is from Aphera
+						$bonusPercent += ColorResource::BONUS_APHERA_TECHNO;
+					}
+
+					# ajout du bonus du lieu
+					$bonusPercent += Game::getImprovementFromScientificCoef($currentBase->planetHistory);
+					$bonus = round($time * $bonusPercent / 100);
+
+					$createdAt =
+						($nbTechnologyQueues === 0)
+							? Utils::now()
+							: $technologyQueues[$nbTechnologyQueues - 1]->getEndedAt()
+					;
+					$tq =
+						(new TechnologyQueue())
+							->setPlayerId($currentPlayer->getId())
+							->setPlaceId($currentBase->getId())
+							->setTechnology($identifier)
+							->setTargetLevel($targetLevel)
+							->setCreatedAt($createdAt)
+							->setEndedAt(Utils::addSecondsToDate($createdAt, round($time - $bonus)))
+					;
+					$technologyQueueManager->add($tq);
+
+					$orbitalBaseManager->decreaseResources($currentBase, $technologyHelper->getInfo($identifier, 'resource', $targetLevel));
+
+					$playerManager->decreaseCredit($currentPlayer, $technologyHelper->getInfo($identifier, 'credit', $targetLevel));
+
+//						if (true === $this->getContainer()->getParameter('data_analysis')) {
+//							$qr = $database->prepare('INSERT INTO
+//							DA_BaseAction(`from`, type, opt1, opt2, weight, dAction)
+//							VALUES(?, ?, ?, ?, ?, ?)'
+//							);
+//							$qr->execute([$session->get('playerId'), 2, $techno, $targetLevel, (DataAnalysis::resourceToStdUnit($technologyHelper->getInfo($techno, 'resource', $targetLevel)) + DataAnalysis::creditToStdUnit($technologyHelper->getInfo($techno, 'credit', $targetLevel))), Utils::now()]);
+//						}
+
+					// alerte
+					$this->addFlash('success', 'Développement de la technologie programmée');
+
+					return $this->redirect($request->headers->get('referer'));
+				} else {
+					throw new ErrorException('les conditions ne sont pas remplies pour développer une technologie');
+				}
+			} else {
+				throw new ErrorException('Cette technologie est déjà en construction');
+			}
+		} else {
+			throw new ErrorException('la technologie indiquée n\'est pas valide');
+		}
+	}
+}
