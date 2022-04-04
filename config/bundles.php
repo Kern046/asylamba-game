1d2deca7d3058ad89f1f59b2de4bc7600fc69cc9
Axel Venet <avenet@wamiz.com>
2022-04-04 02:00:46 +0200

    Require Doctrine ORM pack as a project dependency

--- config/bundles.php
+++ config/bundles.php
@@ -9,4 +9,6 @@ return [
     Sentry\SentryBundle\SentryBundle::class => ['all' => true],
     Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
     Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
+    Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
+    Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
 ];
