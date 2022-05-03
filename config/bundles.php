33ad65692601601934da6d81d89b03b108e06afe
Axel Venet <avenet@wamiz.com>
2022-05-03 23:12:20 +0200

    Require Webpack Encore

--- config/bundles.php
+++ config/bundles.php
@@ -1,11 +1,12 @@
 <?php
 
 return [
-	Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
-	Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
-	Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
-	Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true],
-	Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
-	Sentry\SentryBundle\SentryBundle::class => ['all' => true],
-	Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
+    Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
+    Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
+    Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
+    Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true],
+    Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
+    Sentry\SentryBundle\SentryBundle::class => ['all' => true],
+    Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
+    Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
 ];
