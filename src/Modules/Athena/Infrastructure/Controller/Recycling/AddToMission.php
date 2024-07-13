f3adc0cba0f351e26c83317f90c8874dc8dc032c
Axel Venet <kern046@gmail.com>
2024-07-13 14:25:15 +0200

    Fix recycler ships addition to existing missions

--- src/Modules/Athena/Infrastructure/Controller/Recycling/AddToMission.php
+++ src/Modules/Athena/Infrastructure/Controller/Recycling/AddToMission.php
@@ -2,69 +2,58 @@
 
 namespace App\Modules\Athena\Infrastructure\Controller\Recycling;
 
-use App\Classes\Entity\EntityManager;
-use App\Classes\Exception\ErrorException;
-use App\Classes\Exception\FormException;
+use App\Modules\Athena\Domain\Repository\RecyclingMissionRepositoryInterface;
 use App\Modules\Athena\Helper\OrbitalBaseHelper;
-use App\Modules\Athena\Manager\OrbitalBaseManager;
-use App\Modules\Athena\Manager\RecyclingMissionManager;
 use App\Modules\Athena\Model\OrbitalBase;
 use App\Modules\Athena\Resource\OrbitalBaseResource;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
+use Symfony\Component\HttpKernel\Exception\ConflictHttpException;
+use Symfony\Component\Uid\Uuid;
 
 class AddToMission extends AbstractController
 {
 	public function __invoke(
 		Request $request,
 		OrbitalBase $currentBase,
-		OrbitalBaseManager $orbitalBaseManager,
 		OrbitalBaseHelper $orbitalBaseHelper,
-		RecyclingMissionManager $recyclingMissionManager,
-		EntityManager $entityManager,
-		int $id,
+		RecyclingMissionRepositoryInterface $recyclingMissionRepository,
+		Uuid $id,
 	): Response {
-		$quantity = $request->request->get('quantity');
+		$quantity = $request->request->getInt('quantity');
 
-		if (false !== $quantity) {
-			if ($quantity > 0) {
-				$maxRecyclers = $orbitalBaseHelper->getInfo(OrbitalBaseResource::RECYCLING, 'level', $currentBase->levelRecycling, 'nbRecyclers');
-				$usedRecyclers = 0;
-
-				$baseMissions = $recyclingMissionManager->getBaseActiveMissions($currentBase->getId());
-
-				foreach ($baseMissions as $mission) {
-					$usedRecyclers += $mission->recyclerQuantity + $mission->addToNextMission;
-				}
-
-				if ($maxRecyclers - $usedRecyclers >= $quantity) {
-					$mission = null;
-					foreach ($baseMissions as $baseMission) {
-						if ($baseMission->id === $id && $baseMission->isActive()) {
-							$mission = $baseMission;
-							break;
-						}
-					}
-					if (null !== $mission) {
-						$mission->addToNextMission += $quantity;
-
-						$entityManager->flush($mission);
-
-						$this->addFlash('success', 'Vos recycleurs ont bien été affectés, ils seront ajoutés à la prochaine mission.');
-
-						return $this->redirect($request->headers->get('referer'));
-					} else {
-						throw new ErrorException('Il y a un problème, la mission est introuvable. Veuillez contacter un administrateur.');
-					}
-				} else {
-					throw new ErrorException('Vous n\'avez pas assez de recycleurs libres pour lancer cette mission.');
-				}
-			} else {
-				throw new FormException('Ca va être dur de recycler avec autant peu de recycleurs. Entrez un nombre plus grand que zéro.');
-			}
-		} else {
-			throw new FormException('pas assez d\'informations pour créer une mission de recyclage');
+		if (0 === $quantity) {
+			throw new BadRequestHttpException('Ca va être dur de recycler avec aussi peu de recycleurs. Entrez un nombre plus grand que zéro.');
 		}
+		$maxRecyclers = $orbitalBaseHelper->getInfo(OrbitalBaseResource::RECYCLING, 'level', $currentBase->levelRecycling, 'nbRecyclers');
+		$usedRecyclers = 0;
+
+		$baseMissions = $recyclingMissionRepository->getBaseActiveMissions($currentBase);
+
+		$mission = null;
+		foreach ($baseMissions as $baseMission) {
+			$usedRecyclers += $baseMission->recyclerQuantity + $baseMission->addToNextMission;
+			if ($baseMission->id->equals($id) && $baseMission->isActive()) {
+				$mission = $baseMission;
+			}
+		}
+
+		if (null === $mission) {
+			throw $this->createNotFoundException('Il y a un problème, la mission est introuvable. Veuillez contacter un administrateur.');
+		}
+
+		if ($maxRecyclers - $usedRecyclers < $quantity) {
+			throw new ConflictHttpException('Vous n\'avez pas assez de recycleurs libres pour lancer cette mission.');
+		}
+
+		$mission->addToNextMission += $quantity;
+
+		$recyclingMissionRepository->save($mission);
+
+		$this->addFlash('success', 'Vos recycleurs ont bien été affectés, ils seront ajoutés à la prochaine mission.');
+
+		return $this->redirect($request->headers->get('referer'));
 	}
 }
