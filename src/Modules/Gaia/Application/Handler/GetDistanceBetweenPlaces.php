227bca4d463b34574652426dbd4bb5cdb1c9c647
Axel Venet <kern046@gmail.com>
2024-07-16 11:16:03 +0200

    Fix distance calculation between places

--- src/Modules/Gaia/Application/Handler/GetDistanceBetweenPlaces.php
+++ src/Modules/Gaia/Application/Handler/GetDistanceBetweenPlaces.php
@@ -2,7 +2,6 @@
 
 namespace App\Modules\Gaia\Application\Handler;
 
-use App\Classes\Library\Game;
 use App\Modules\Gaia\Model\Place;
 
 readonly class GetDistanceBetweenPlaces
@@ -12,14 +11,14 @@ readonly class GetDistanceBetweenPlaces
 		return $this->getDistance(
 			$from->system->xPosition,
 			$to->system->xPosition,
-			$from->system->xPosition,
-			$to->system->xPosition,
+			$from->system->yPosition,
+			$to->system->yPosition,
 		);
 	}
 
 	private function getDistance(int $xa, int $xb, int $ya, int $yb): int
 	{
-		$distance = intval(floor(sqrt((($xa - $xb) * ($xa - $xb)) + (($ya - $yb) * ($ya - $yb)))));
+		$distance = intval(floor(sqrt(pow($xa - $xb, 2) + pow($ya - $yb, 2))));
 
 		return max(1, $distance);
 	}
