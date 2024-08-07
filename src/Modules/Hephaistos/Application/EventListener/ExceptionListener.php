ac99088c236b6609dcbf513c62a96d02a4d2d4d2
Axel Venet <kern046@gmail.com>
2024-08-07 14:48:27 +0200

    Implement exception listener to set error flash message and redirect to referer URL

---
+++ src/Modules/Hephaistos/Application/EventListener/ExceptionListener.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Modules\Hephaistos\Application\EventListener;
+
+use Symfony\Component\EventDispatcher\Attribute\AsEventListener;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Event\ExceptionEvent;
+use Symfony\Component\HttpKernel\Exception\HttpExceptionInterface;
+
+#[AsEventListener]
+readonly class ExceptionListener
+{
+	public function __invoke(ExceptionEvent $event): void
+	{
+		$exception = $event->getThrowable();
+		if (!$exception instanceof HttpExceptionInterface) {
+			return;
+		}
+
+		$request = $event->getRequest();
+		if (null === ($referer = $request->headers->get('referer'))) {
+			return;
+		}
+
+		$flashCode = match ($exception->getStatusCode()) {
+			Response::HTTP_CONFLICT => '103',
+			default => 'error',
+		};
+
+		$request->getSession()->getFlashBag()->add($flashCode, $exception->getMessage());
+
+		$event->setResponse(new RedirectResponse($referer));
+	}
+}
